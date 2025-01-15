73df8f58c04bd8cf6961990ef1f2f97ef7108e85
Hierakonpolis <riccardefe@gmail.com>
2025-01-15 12:57:30 +0300

    Exclude fieldmaps if made specifically for DWI only with a different sequence

--- appdir_scripts/sequence_level_tasks.sh
+++ appdir_scripts/sequence_level_tasks.sh
@@ -11,7 +11,7 @@ phasediff_file=$(find /fmap -type f -name "sub-*_ses-*_acq-*_phasediff.nii.gz")
 phasediff_file=$(basename "$phasediff_file")
 AP_file=$(find /fmap -type f -name "sub-*_ses-*_acq-*_dir-AP_epi.nii.gz")
 AP_file=$(basename "$AP_file")
-PA_file=$(find /fmap -type f -name "sub-*_ses-*_acq-*_dir-PA_epi.nii.gz")
+PA_file=$(find /fmap -type f -name "sub-*_ses-*_acq-[0-9]*_dir-PA_epi.nii.gz" ! -name "*DWI*")
 PA_file=$(basename "$PA_file")
 
 anat_file=$(find /anat -type f -name "sub-*_ses-*_acq-*_T1w.nii.gz")
@@ -26,4 +26,4 @@ for func_task in /func/*.nii.gz; do
   else
       ./one_task_correction.sh -f "${func_file}" -a "${anat_file}"
   fi
-  done
+done
