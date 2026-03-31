bcc53fbe7cb565c23de79c0a0419d67ebf5428e8
Hierakonpolis <riccardefe@gmail.com>
2026-03-31 17:16:57 +0300

    bondi data to MNI 2mm

---
+++ scripts/resample_all_bondi_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+fmri_dir=/mnt/data0/Bondi/preprocessed_fmri
+dir_2mm=${fmri_dir}/2mm
+mkdir -p dir_2mm
+
+tsp -S 16
+for scan in "${fmri_dir}"/*.nii; do
+  scan_name=$(basename $scan)
+  out_name="${scan_name%.nii}_2mm.nii.gz"
+
+      tsp docker run \
+        -v "${dir_2mm}":/out \
+        -v "${fmri_dir}":/func \
+        --rm neurobox:latest \
+        flirt \
+          -in "/func/${scan_name}" \
+          -ref "/opt/fsl/data/standard/MNI152_T1_2mm.nii.gz" \
+          -out "/out/${out_name}" \
+          -applyxfm \
+          -usesqform
+done
+
+tsp tsp -S 1
